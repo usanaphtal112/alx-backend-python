613ef7ea2d19430b06bb91982bd27eb61bfa5b1f
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2024-07-03 00:01:02 -0700

    Formated files using black

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-'''Modules of task-0.
-'''
+"""Modules of task-0.
+"""
+
+
 def add(a: float, b: float) -> float:
     """
     Adds two floating-point numbers and returns the result.
