45537c99a5a5c7e89f414980532a410593bcc347
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2024-07-02 23:46:24 -0700

    Added project files

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''Modules of task-0.
+'''
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers and returns the result.
+
+    Parameters:
+    a (float): The first number to be added.
+    b (float): The second number to be added.
+
+    Returns:
+    float: The sum of the two input numbers.
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,4 @@
+# 0x00. Python - Variable Annotations
+
+(image url)[https://i.redd.it/y9y25tefi5401.png]
+![ALX BACKEND](https://i.redd.it/y9y25tefi5401.png "ALX PYTHON BACKEND")
